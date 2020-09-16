21f3d6db02e306393c6c0622fe688648b285f3ad
Natalia670 <A01338719@itesm.mx>
2020-09-16 17:59:20 -0500

    Added git ignore

--- models/user.js
+++ models/user.js
@@ -45,8 +45,3 @@ module.exports.comparePassword = function(candidatePassword, hash, callback){
       callback(null, isMatch);
     });
   }
-// Custom validation for email
-/*userSchema.path('email').validate((val) => {
-    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailRegex.test(val);
-}, 'Invalid e-mail.');*/
