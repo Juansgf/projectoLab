ffc046f1cf61afbb90e92a6ce8d1455e924078f6
NanMed <46913776+NanMed@users.noreply.github.com>
2020-11-10 21:24:44 -0600

    Corrección filtro admin

--- proyectoAng/src/app/components/show-posts/show-post.component.ts
+++ proyectoAng/src/app/components/show-posts/show-post.component.ts
@@ -99,30 +99,29 @@ export class ShowPostComponent implements OnInit {
 
   getAdminPost(){
     this.showPostService.getAllAdminPost().subscribe(result => {
-      console.log("Picado")
-      const temp = result['post'];
       this.posts = result['post'];
       // console.log(result)
       console.log("TODOS LOS POSTS ADMIN",this.posts)
 
-      // this.authService.authenticatePorfile().subscribe(profile => {
-      //   this.id = profile.body.user._id
+      this.authService.authenticatePorfile().subscribe(profile => {
+        this.id = profile.body.user._id
 
-      //   const diffPost = this.posts.map(person => person.createdBy == this.id);
-      //   console.log(diffPost)
+        const diffPost = this.posts.map(person => person.createdBy == this.id);
+        console.log(diffPost)
 
-      //   for(var i = 0; i < diffPost.length; i++){
-      //     if(diffPost[i] = true){
+        for(var i = 0; i < diffPost.length; i++){
+          if(diffPost[i] = true){
 
-      //     }
-      //   }
-      // })
+          }
+        }
+      })
     });
   }
 
   likePost(post){
     console.log("likes", post);
       this.authService.likePost(post).subscribe(data => {
+        console.log('Dentro de like', data)
         this.getAllPost();
     });
 
